c19f2b8fa98ea099be0786bbef4490ad5fefc3c4
cmsanner <craftbloggerfozzie@gmail.com>
2020-06-18 16:03:02 -0500

    get image request working

--- server.js
+++ server.js
@@ -65,6 +65,21 @@ app.get('/profile/:id', (req,res) => {
     if (!found){
         res.status(400).json('user not found');
     }
+})
+
+app.post('/image',(req,res) => {
+    const {id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id){
+            found = true;
+            user.entries ++;
+           return res.json(user.entries);
+        }
+    })
+    if (!found){
+        res.status(400).json('user not found');
+    }
 
 })
 
