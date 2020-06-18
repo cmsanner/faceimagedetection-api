aad558ea9f8a7aeaf529f6f10daeac9da270eb6e
cmsanner <craftbloggerfozzie@gmail.com>
2020-06-18 15:13:46 -0500

    hard code some test data

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
 const port = 3000;
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+
+    ]
+}
 
 //create route to test get/post is working using postman
 app.get('/', (req, res)=>{
     // send response to postman
     res.send('this is working!');
 } )
+
+app.post('/login', (req, res) =>{
+    // res.send('login');
+    if(req.body.emailAddress === database.users[0].email && req.body.password === database.users[0].password){
+        res.json('success');
+    }else{
+        res.status(400).json('error logging in');
+    }
+    
+})
 
 app.listen(port, ()=>{
     console.log('app is running on port: ', port);
@@ -15,7 +48,7 @@ app.listen(port, ()=>{
 
 /* ROUTES 
 * --> res = this is working!
-* /signin --> POST = success/fail
+* /login --> POST = success/fail
 * /register --> POST = user
 * /profile/:userId -->  GET = user
 * /image --> PUT --> user
