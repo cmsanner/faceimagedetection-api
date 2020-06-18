b88ce7d95a3c9942ae4fbebf2568a2c93ae97ef3
cmsanner <craftbloggerfozzie@gmail.com>
2020-06-18 15:32:37 -0500

    get register working

--- server.js
+++ server.js
@@ -38,7 +38,20 @@ app.post('/login', (req, res) =>{
     }else{
         res.status(400).json('error logging in');
     }
-    
+})
+
+app.post('/register', (req,res) => {
+    const {email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()  
+    });
+    res.json(database.users[database.users.length-1]);
+
 })
 
 app.listen(port, ()=>{
