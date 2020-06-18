442241fabf7fb73b4d37d1337160398791606898
cmsanner <craftbloggerfozzie@gmail.com>
2020-06-18 14:05:48 -0500

    test express and postman are working

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//create route to test get/post is working using postman
+app.get('/', (req, res)=>{
+    // send response to postman
+    res.send('this is working!');
+} )
+
 app.listen(port, ()=>{
     console.log('app is running on port: ', port);
 })
